1813220220ca2067bb67ae34fd69872e3c6d1ce5
Miliyonayalew <miliayalew@gmail.com>
2022-11-02 14:23:19 +0300

    deployed the page and update README.md

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import TodoContainer from './Components/TodoContainer';
 
 function App() {
